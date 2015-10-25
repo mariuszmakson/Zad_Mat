222725d4ae14c04c1842ed20411e6075287a26e9
mariuszmaksoń <mariusz.max@op.pl>
2015-10-25 13:26:51 +0100

    pierwszy zapis

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //
+        System.out.println("Program licz¹cy");
+        Double a, b, c, wynik;
+        a=2.0;
+        b=30.0;
+        c=4.2;
+
+        wynik = a+b+c;//
+        System.out.println(wynik);//
+
+    }
+}
