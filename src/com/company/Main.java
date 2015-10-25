a721b87c05abe466fd5d88faf8cc6f2224552078
mariuszmaksoń <mariusz.max@op.pl>
2015-10-25 22:00:53 +0100

    dodany opis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,22 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //
+
+//Zadaniem tego programu jest liczenie sumy dla trzech zmiennych
+
+//Najpierw definiujemy zmienne
         System.out.println("Program licz¹cy");
         Double a, b, c, wynik;
         a=2.0;
         b=30.0;
         c=4.2;
 
-        wynik = a+b+c;//
-        System.out.println(wynik);//
+        //W tym miejscu podstawiamy dowolne zmienne rzeczywiste
+
+
+        wynik = a+b+c;
+        System.out.println(wynik);//W wyniku podstawienia dowolnych wartoœci otrzymujemy wartoœæ spe³niaj¹com zadany wzór
+
 
     }
 }
